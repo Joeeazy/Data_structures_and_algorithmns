2c4a5074a1bfda3f541f1bf3e79452f4b2a093b4
Joeeazy <joeeazy34@gmail.com>
2024-04-13 23:14:58 +0300

    math algos

---
+++ math_algorithmns/fibonacci_sequence.js
@@ -0,0 +1,3 @@
+// given a number "n", find the first "n" elements of the fibonacci sequence
+//fibonacci sequence is a sequence in which each number is the sum of the precceding ones
+//exaample fibonacci(2) = [0,1] fibonacci(3) = [0,1,1]
